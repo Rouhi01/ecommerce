b6b86e1804786cac847420d4a107758853344747
rouhi <r.shafahi11@gmail.com>
2024-07-03 14:05:24 +0330

    build drf first configurations

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -18,6 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External Packages
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -90,3 +93,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+
+}
